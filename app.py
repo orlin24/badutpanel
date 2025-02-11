bb223cbbe06673254f2deb111beaee9623cebd0d
orlin24 <37975410+orlin24@users.noreply.github.com>
2025-02-11 18:54:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from models import db
 from config import config
 from admin import create_admin_app
